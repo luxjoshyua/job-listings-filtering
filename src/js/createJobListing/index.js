cbdf524f1e9ac25f9393a81fd5635d345b7d8d69
Joshua Fielding <Joshua.Fielding@fairfaxmedia.com.au>
2020-06-03 17:30:33 +1000

    added data attributes in

--- src/js/createJobListing/index.js
+++ src/js/createJobListing/index.js
@@ -1,24 +1,38 @@
 var json = require("../data.json");
 var data = json;
-// console.log(data);
-// import img1 from "../../assets/images/photosnap.svg";
+
+var db = {
+  company: [],
+  position: [],
+  role: [],
+  level: [],
+  contract: [],
+  location: [],
+  languages: [],
+  tool: [],
+};
 
 // populate the job boards
 const jobsContainer = document.querySelector(".jobs-wrapper");
 const createJobPosting = () => {
   data.forEach((job) => {
-    // console.log(job);
     const jobListing = document.querySelector(".job-item");
     const clone = jobListing.cloneNode(true);
     jobsContainer.appendChild(clone);
+    // console.log(job);
 
     // set image
-    // console.log(job.logo);
     clone.querySelector(".logo-outer img").setAttribute("src", `${job.logo}`);
     clone.querySelector(".logo-outer img").setAttribute("width", "100%");
 
     // set company name
     clone.querySelector(".company").innerHTML = `${job.company}`;
+    clone.setAttribute(
+      "data-db",
+      clone.getAttribute("data-db")
+        ? clone.getAttribute("data-db") + " " + job.company.toLowerCase()
+        : job.company.toLowerCase()
+    );
 
     // set new status
     if (job.new) {
@@ -39,13 +53,33 @@ const createJobPosting = () => {
 
     // set job title
     clone.querySelector(".job-title").innerHTML = `${job.position}`;
+    clone.setAttribute(
+      "data-db",
+      clone.getAttribute("data-db")
+        ? clone.getAttribute("data-db") + " " + job.position.toLowerCase()
+        : job.position.toLowerCase()
+    );
 
     // set posted at
     clone.querySelector(".posted-at").innerHTML = `${job.postedAt}`;
+
     // set contract
     clone.querySelector(".contract").innerHTML = `${job.contract}`;
+    clone.setAttribute(
+      "data-db",
+      clone.getAttribute("data-db")
+        ? clone.getAttribute("data-db") + " " + job.contract.toLowerCase()
+        : job.contract.toLowerCase()
+    );
+
     // set location
     clone.querySelector(".location").innerHTML = `${job.location}`;
+    clone.setAttribute(
+      "data-db",
+      clone.getAttribute("data-db")
+        ? clone.getAttribute("data-db") + " " + job.location.toLowerCase()
+        : job.location.toLowerCase()
+    );
 
     // set role
     const roleSpan = document.createElement("span");
@@ -53,6 +87,12 @@ const createJobPosting = () => {
     roleSpan.className = `${job.role}`;
     roleSpan.classList.add("roles-background");
     clone.querySelector(".roles-wrapper").appendChild(roleSpan);
+    clone.setAttribute(
+      "data-db",
+      clone.getAttribute("data-db")
+        ? clone.getAttribute("data-db") + " " + job.role.toLowerCase()
+        : job.role.toLowerCase()
+    );
 
     // set level
     const levelSpan = document.createElement("span");
@@ -60,6 +100,11 @@ const createJobPosting = () => {
     levelSpan.className = `${job.level}`;
     levelSpan.classList.add("levels-background");
     clone.querySelector(".levels-wrapper").appendChild(levelSpan);
+    clone.setAttribute(
+      "data-db",
+      clone.getAttribute("data-db"),
+      clone.getAttribute("data-db") + " " + job.level.toLowerCase()
+    );
 
     // set languages
     job.languages.forEach((language) => {
@@ -68,6 +113,13 @@ const createJobPosting = () => {
       languageSpan.className = `${language}`;
       languageSpan.classList.add("languages-background");
       clone.querySelector(".languages-wrapper").appendChild(languageSpan);
+      clone.setAttribute(
+        "data-db",
+        clone.getAttribute("data-db")
+          ? clone.getAttribute("data-db") + " " + language.toLowerCase()
+          : language.toLowerCase()
+      );
+      db.languages.push(language);
     });
 
     // set tools
@@ -77,8 +129,31 @@ const createJobPosting = () => {
       toolSpan.className = `${tool}`;
       toolSpan.classList.add("tools-background");
       clone.querySelector(".tools-wrapper").appendChild(toolSpan);
+      clone.setAttribute(
+        "data-db",
+        clone.getAttribute("data-db")
+          ? clone.getAttribute("data-db") + " " + tool.toLowerCase()
+          : tool.toLowerCase()
+      );
+      db.tool.push(tool);
     });
+
+    // new object here {}
+    db.company.push(job.company);
+    db.position.push(job.position);
+    db.contract.push(job.contract);
+    db.location.push(job.location);
+    db.role.push(job.role);
+    db.level.push(job.level);
+    job.dom = clone;
+    // console.log(db);
   });
 };
 
+// once populated, remove the duplicates in the db object
+
+// with languages and tools, use splice to separate the array into strings so can then remove duplicates e.g. HTML occurring thrice
+
 createJobPosting();
+
+// console.log(data[2]);
--- src/js/createSearchFunctionality/index.js
+++ src/js/createSearchFunctionality/index.js
@@ -1,15 +1,12 @@
 const jobListing = document.querySelectorAll(".job-item");
 
-// role function
-// jobListing.forEach((job) => {
-//   const roleSpan = document.querySelectorAll(".roles-wrapper span");
-//   roleSpan.forEach((role) => {
-//     role.addEventListener("click", (e) => {
-//       console.log("click", e.target);
-//     });
-//   });
-// });
-
 // level function
 
 // languages function
+
+// loop through json feed
+// get company, position, role, level, contract, location, languages, tools
+
+// new object
+
+// then remove duplicates
